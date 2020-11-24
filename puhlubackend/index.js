ff6bc03c6ba79595f57a5078b3f2382d447ef062
tpjhamas <tomi.pj.hamari@student.jyu.fi>
2020-11-24 20:36:31 +0200

    Osa 3 viimesiset tehtävät

--- puhlubackend/index.js
+++ puhlubackend/index.js
@@ -1,20 +1,27 @@
-const express = require('express')
+require("dotenv").config()
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const Person = require("./models/person")
+const { request, response } = require("express")
+
 const app = express()
+// const morgan = require("morgan")
 
-const cors = require('cors')
-const morgan = require("morgan")
+// morgan.token("response", function getResp ( res ) {
+//   console.log(res.body);
 
-morgan.token("response", function getResp ( res ) {
-  console.log(res.body);
-  
-  return JSON.stringify(res.body)
-})
-
+//   return JSON.stringify(res.body)
+// })
 
 app.use(cors())
+app.use(express.static("build"))
 app.use(express.json())
-app.use(express.static('build'))
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :response"))
+// app.use(logger)
+// app.use(morgan(":method :url :status :res[content-length] - :response-time ms :response"))
+
 
 let persons = [
   {
@@ -48,80 +55,93 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>")
 })
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons)
+app.get("/api/persons", (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  
-  if ( person ) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if ( person ) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id).then(result => {
+    response.status(204).end()
+  })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post("/api/persons", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body
-  
-  if (!body.name) {
-    return response.status(400).json({
-      error: "Name missing"
-    })
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: "Number missing"
-    })
-  }
-
-  const tmpPerson = persons.find(p => p.name === body.name)
-
-  if (tmpPerson) {
-    return response.status(400).json({
-      error: "Name must be unique"
-    })
-  }
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(),
   }
 
-  persons = persons.concat(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
-  response.json(persons);
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
+  })
+    .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<div>
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date().toString()}</p>
-    </div>`
-  )
+  Person.find({}).then(persons => {
+    response.send(
+      `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date().toString()}</p>
+      </div>`
+    )
+  })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "Unknown endpoint" })
+}
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`)
 })
--- puhlubackend/puhlufrontend/src/services/people.js
+++ puhlubackend/puhlufrontend/src/services/people.js
@@ -1,20 +1,24 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-    return axios.get(baseUrl)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 const create = newObject => {
-    return axios.post(baseUrl, newObject);
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
 }
 
 const deleteDude = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
 }
 
 export default {
