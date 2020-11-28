4610c79ac3b83db377e3495846e9c08d61285b51
tpjhamas <tomi.pj.hamari@student.jyu.fi>
2020-11-28 23:11:00 +0200

    Puhelinluettelolle osa4 muutokset
    Puhelinluettelo kahdessa kansiossa, toisessa osa3 versio ja toisessa osa4 muutokset tehtynä

---
+++ puhlubackendOsa4/build/precache-manifest.cf4e0a2eb02f84d2dcea713d98737a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f50f770b9c62ef204a5afc0cfb99bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86c5fca0da678fa72dc0",
+    "url": "/static/css/main.aa177f17.chunk.css"
+  },
+  {
+    "revision": "72729f8c5b9726d368f5",
+    "url": "/static/js/2.2701b9d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2701b9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c5fca0da678fa72dc0",
+    "url": "/static/js/main.8d49ba83.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
